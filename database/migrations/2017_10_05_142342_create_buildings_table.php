9d67ba969457769e35fffc57a91b9ee9e434d9b6
ibrahemhamdy1 <mslm356@gmail.com>
2017-10-05 16:51:22 +0200

    after adding  migration  to  building  table

---
+++ database/migrations/2017_10_05_142342_create_buildings_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBuildingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('buildings', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('bu_name',100);
+            $table->string('bu_price',20);
+            $table->smallInteger('bu_rent');
+            $table->string('bu_square',10);
+            $table->smallInteger('bu_type');
+            $table->string('bu_small_dis',160);
+            $table->string('bu_meta',200);
+            $table->string('bu_longitude',50);
+            $table->string('bu_latitude',50);
+            $table->longText('bu_large_dis');
+            $table->smallInteger('bu_status');
+            $table->timestamps();
+            $table->integer('user_id');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('buildings');
+    }
+}
